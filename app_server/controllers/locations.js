e83f5e5afd615e6c26e633389ce83eec63f264e3
Hugh <hugh@quanta-solutions.com>
2020-02-10 16:17:36 +0000

    Updated functionality

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,24 @@
+/* GET 'home' page */
+const homelist = (req, res) =>
+{
+    res.render('locations-list', { title: 'Home' });
+};
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) =>
+{
+    res.render('location-info', { title: 'Location Info' });
+};
+
+/* GET 'Add review' page */
+const addReview = (req, res) =>
+{
+    res.render('location-review-form', { title: 'Add Review' });
+};
+
+module.exports =
+{
+    homelist,
+    locationInfo,
+    addReview
+};
