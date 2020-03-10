a8a189b4f1d83f8e0ded4f011262b9694b7595ac
Hugh <hugh@quanta-solutions.com>
2020-03-10 23:22:51 +0000

    Updated functionality

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,7 +1,11 @@
 /* GET 'about' page */
 const about = (req, res) =>
 {
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text',
+    {
+        title:   'About',
+        content: 'Loc8r is being created to help people find places with wifi access, where they can sit down and get a bit of work done.<br /><br />This site is currently under development.'
+    });
 };
 
 module.exports =
